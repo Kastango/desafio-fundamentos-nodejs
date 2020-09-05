fb16949d0ef9a19194b23a47e0d4ea47c5a20d2b
Arthur <Metroidling@gmail.com>
2020-09-05 16:14:25 -0300

    started

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,16 +1,26 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
-class CreateTransactionService {
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
+export default class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
   constructor(transactionsRepository: TransactionsRepository) {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({ title, value, type }: Request): Transaction {
+    const transaction = this.transactionsRepository.create({
+      title,
+      value,
+      type,
+    });
+
+    return transaction;
   }
 }
-
-export default CreateTransactionService;
