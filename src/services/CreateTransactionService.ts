aa0acadd5c773c15cf069a93609443f6704124f8
Arthur Sosnowski <Metroidling@gmail.com>
2020-10-03 17:50:09 -0300

    all tests passed

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -15,6 +15,12 @@ export default class CreateTransactionService {
   }
 
   public execute({ title, value, type }: Request): Transaction {
+    const { total } = this.transactionsRepository.getBalance();
+
+    if ( type === "outcome" && total < value ) {
+      throw new Error('You do not have enough balance');
+    }
+
     const transaction = this.transactionsRepository.create({
       title,
       value,
